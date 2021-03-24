8c6c0407654c6e08eef42aac0c0f20e12af1b5bb
Haris Rio <harisbaskar1998@gmail.com>
2021-03-24 09:39:25 +0530

    List all vets on vet index page. Closes #15.

--- pet-clinic-web/src/main/java/org/springframework/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/springframework/springpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package org.springframework.springpetclinic.controllers;
 
+import org.springframework.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listVet() {
+	public String listVet(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 
