81a91f78d36833b54937ab0607a00c9339670fde
Haris Rio <harisbaskar1998@gmail.com>
2021-03-29 11:33:20 +0530

    Added Vet Specialty Entity, Associate To Vet. Closes #27

--- pet-clinic-data/src/main/java/org/springframework/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/springframework/springpetclinic/model/Vet.java
@@ -1,5 +1,17 @@
 package org.springframework.springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+	private Set<Speciality> specialities;
+
+	public Set<Speciality> getSpecialities() {
+		return specialities;
+	}
+
+	public void setSpeialities(Set<Speciality> specialities) {
+		this.specialities = specialities;
+	}
 
 }
