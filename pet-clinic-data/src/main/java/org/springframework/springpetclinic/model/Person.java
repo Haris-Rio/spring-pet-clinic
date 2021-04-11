a46311546ec4e490cce3ce743527eae601381062
Haris Rio <harisbaskar1998@gmail.com>
2021-04-11 20:06:36 +0530

    Add Project Lambok and Refactor. Closes #51

--- pet-clinic-data/src/main/java/org/springframework/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/org/springframework/springpetclinic/model/Person.java
@@ -3,6 +3,15 @@ package org.springframework.springpetclinic.model;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
 	
@@ -11,20 +20,10 @@ public class Person extends BaseEntity{
 	@Column(name = "last_name")
 	private String lastName;
 	
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
+	public Person(Long id, String firstName, String lastName) {
+		super(id);
 		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	
 
 }
